5021b23828142c3d283fb5281f1725f9e28932e0
shazkun <seanwiltonr@gmail.com>
2025-05-20 14:36:14 +0800

    test ios build

--- lib/screen/auth/login/login_screen.dart
+++ lib/screen/auth/login/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:learnbound/screen/auth/login/login_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'login_functions.dart';
 
